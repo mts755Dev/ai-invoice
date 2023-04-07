997f7c15b35de2712b7aad1e54b8a49f51b4304a
talha saleem <Talha.saleem@devsinc.com>
2023-04-07 23:47:48 +0500

    before pdf generation functionality

---
+++ src/components/Home.js
@@ -0,0 +1,228 @@
+import { useState, useRef, useEffect } from 'react';
+import styles from '../styles/home.module.css';
+import ReactMarkdown from 'react-markdown';
+import user from '../assets/user.png';
+import gptIcon from '../assets/chatgpt-icon.png';
+import { Button } from '@mui/material';
+import CircularProgress from '@mui/material/CircularProgress';
+window.SpeechRecognition = window.webkitSpeechRecognition || window.SpeechRecognition;
+
+export default function Home() {
+
+  const apiKey = process.env.REACT_APP_OPENAI_API_KEY;
+  const [pdfData, setPdfData] = useState(false);
+  const [userInput, setUserInput] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [recognition, setRecognition] = useState(null);
+  const [isRecording, setIsRecording] = useState(false);
+  const [messages, setMessages] = useState([
+    { role: "assistant", content: "Hi, I'm here to generate you an invoice. Share the details..." }
+  ]);
+
+  const messageListRef = useRef(null);
+  const textAreaRef = useRef(null);
+
+  useEffect(() => {
+    const newRecognition = new window.SpeechRecognition();
+    setRecognition(newRecognition);
+  }, []);
+
+  // Auto scroll chat to bottom
+  useEffect(() => {
+    const messageList = messageListRef.current;
+    messageList.scrollTop = messageList.scrollHeight;
+  }, [messages]);
+
+  // Focus on text field on load
+  useEffect(() => {
+    textAreaRef.current.focus();
+  }, []);
+
+  // Handle errors
+  const handleError = () => {
+    setMessages((prevMessages) => [...prevMessages, { role: "assistant", content: "To get started, make sure internet connectivity." }]);
+    setLoading(false);
+    recognition.stop();
+    setIsRecording(false);
+    setUserInput("");
+  };
+
+  const startVoiceRecognition = () => {
+    let previousTranscriptions = "";
+    recognition.continuous = true;
+    recognition.start();
+    setIsRecording(true);
+    setMessages((prevMessages) => [
+      ...prevMessages,
+      { role: "user", content: "Listening..." },
+    ]);
+
+    recognition.onresult = (event) => {
+      const resultIndex = event.resultIndex;
+      const transcript = event.results[resultIndex][0].transcript;
+      previousTranscriptions += transcript;
+      setUserInput(previousTranscriptions.trim());
+    };
+
+    recognition.onend = () => {
+      setLoading(false);
+      recognition.stop();
+      setIsRecording(false);
+      handleSubmit();
+    };
+
+    recognition.onerror = () => {
+      setLoading(false);
+      recognition.stop();
+      setIsRecording(false);
+      setMessages((prevMessages) => [
+        ...prevMessages,
+        {
+          role: "assistant",
+          content:
+            "Sorry, I could not understand what you said. Please try again.",
+        },
+      ]);
+    };
+  };
+
+  // Handle form submission
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (userInput.trim() === "") {
+      return;
+    }
+    setLoading(true);
+    setIsRecording(false);
+    recognition.stop();
+    const context = [...messages, { role: "user", content: userInput }];
+    setMessages(context);
+    const apiRequestBody = {
+      "model": "gpt-3.5-turbo",
+      messages: [{ "role": "system", "content": "Act as JSON invoice generator, don't answer questions other than related to invoice. Ask user about: Date, Invoice No, Invoiced To, Pay To, Service, Description, Rate, QTY, Amount, Sub Total, Tax, Total. You have to ask user if any of these detail are missing. When the user says no, then you just have to return data as JSON and a button to generate pdf." }].concat(context),
+      temperature: 0.1,
+      max_tokens: 200
+    }
+    // Send chat history to API
+    const response = await fetch("https://api.openai.com/v1/chat/completions",
+      {
+        method: "POST",
+        headers: {
+          "Authorization": "Bearer " + apiKey,
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(apiRequestBody)
+      });
+
+    if (!response.ok) {
+      handleError();
+      return;
+    }
+    // Reset user input
+    setUserInput("");
+    const data = await response.json();
+    const jsonRegex = /^\s*(\{|\[)[\s\S]*([\}|\]])\s*$/;
+    if (jsonRegex.test(data.choices[0].message.content)) {
+      setPdfData(true);
+    }
+    setPdfData(data.choices[0].message.content)
+    setMessages((prevMessages) => [...prevMessages, { role: "assistant", content: data.choices[0].message.content }]);
+    setLoading(false);
+
+  };
+
+  // Prevent blank submissions and allow for multiline input
+  const handleEnter = (e) => {
+    if (e.key === "Enter" && userInput) {
+      if (!e.shiftKey && userInput) {
+        if (!recognition || (recognition && !recognition.active)) {
+          handleSubmit(e);
+        }
+      }
+    } else if (e.key === "Enter") {
+      e.preventDefault();
+    }
+  };
+
+  return (
+    <>
+      <div className={styles.topnav}>
+        <div className={styles.navlogo}>
+          <a href="/">Invoice Bot</a>
+        </div>
+      </div>
+      <main className={styles.main}>
+        <div className={styles.cloud}>
+          <div ref={messageListRef} className={styles.messagelist}>
+            {messages.map((message, index) => {
+              return (
+                // The latest message sent by the user will be animated while waiting for a response
+                <div key={index} className={message.role === "user" && loading && index === messages.length - 1 ? styles.usermessagewaiting : message.role === "assistant" ? styles.apimessage : styles.usermessage}>
+                  {/* Display the correct icon depending on the message type */}
+                  {message.role === "assistant" ? <img src={gptIcon} alt="AI" width="30" height="30" className={styles.boticon} priority={true} /> : <img src={user} alt="Me" width="30" height="30" className={styles.usericon} priority={true} />}
+                  <div className={styles.markdownanswer}>
+                    {/* Messages are being rendered in Markdown format */}
+                    <ReactMarkdown linkTarget={"_blank"}>{message.content}</ReactMarkdown>
+                  </div>
+                </div>
+              )
+            })}
+          </div>
+        </div>
+        {pdfData && <Button className={styles.bttu} variant="contained">Generate PDF</Button>}
+        <div className={styles.center}>
+          <div className={styles.cloudform}>
+            <form onSubmit={handleSubmit}>
+              <textarea
+                disabled={loading}
+                onKeyDown={handleEnter}
+                ref={textAreaRef}
+                autoFocus={false}
+                rows={1}
+                maxLength={512}
+                type="text"
+                id="userInput"
+                name="userInput"
+                placeholder={loading ? "Waiting for response..." : "Type your question..."}
+                value={userInput}
+                onChange={e => setUserInput(e.target.value)}
+                className={styles.textarea}
+              />
+              <button
+                type="submit"
+                disabled={loading}
+                className={styles.generatebutton}
+              >
+                {loading ? <div className={styles.loadingwheel}><CircularProgress color="inherit" size={20} /> </div> :
+                  // Send icon SVG in input field
+                  <svg viewBox='0 0 20 20' className={styles.svgicon} xmlns='http://www.w3.org/2000/svg'>
+                    <path d='M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z'></path>
+                  </svg>}
+              </button>
+              {isRecording ?
+                <button className={styles.voicebutton} onClick={() => {
+                  recognition.stop();
+                  setIsRecording(false);
+                }}>
+                  <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className={styles.svgicon}>
+                    <circle cx="12" cy="12" r="10" fill="green" />
+                  </svg>
+                </button> :
+                <button
+                  type="button"
+                  onClick={startVoiceRecognition}
+                  className={styles.voicebutton}
+                >
+                  <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className={styles.svgicon}>
+                    <circle cx="12" cy="12" r="7" fill="#ff0000" />
+                    <circle cx="12" cy="12" r="10" stroke="#ff0000" strokeWidth="3" fill="none" />
+                  </svg>
+                </button>}
+            </form>
+          </div>
+        </div>
+      </main>
+    </>
+  )
+}
