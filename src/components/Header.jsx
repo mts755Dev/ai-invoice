549344f9c3ce76ea241a355bd866f52b25045c93
talha saleem <Talha.saleem@devsinc.com>
2023-04-11 00:50:08 +0500

    added logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import styles from '../styles/home.module.css';
-
+import logo from '../assets/bot.png';
 function Header() {
   return (
     <div className={styles.topnav}>
+      <img src={logo} alt="Logo" style={{ height: "30px"}} />
     <div className={styles.navlogo}>
       <a href="/">Invoice Bot</a>
     </div>
